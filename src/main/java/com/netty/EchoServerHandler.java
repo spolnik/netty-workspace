446c1e14f435cd22ce08dec1ab50b236c380eef6
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-04-21 22:35:39 +0200

    - add skeleton project - add Echo Server & Client for Netty

---
+++ src/main/java/com/netty/EchoServerHandler.java
@@ -0,0 +1,30 @@
+package com.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+
+import static io.netty.util.CharsetUtil.UTF_8;
+
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString(UTF_8));
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
